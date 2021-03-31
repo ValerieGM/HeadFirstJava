6f8817f79e4772c66c29783fe2edae4e14161582
ValerieGM <vgongora@student.wethinkcode.co.za>
2021-03-31 22:43:22 +0200

    Add: Chapter 1

---
+++ Chapter1/Exercise1C.java
@@ -0,0 +1,12 @@
+public class Exercise1C {
+    // Main method needed to run
+    public static void main(String[] args) {
+        int x = 5;
+        while (x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("small x");
+            }
+        }
+    }
+}
