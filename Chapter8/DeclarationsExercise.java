9df23c31e56b842b83ba65cf178c9230018becde
ValerieGM <vgongora@student.wethinkcode.co.za>
2021-04-08 10:30:41 +0200

    Add: Chapter 8

---
+++ Chapter8/DeclarationsExercise.java
@@ -0,0 +1,23 @@
+public class DeclarationsExercise {
+    public static void main(String[] args) {}
+}
+
+/* 2 */
+abstract class Top {}
+class Tip extends Top {}
+
+/* 3 */
+abstract class Fee {}
+abstract class Fi extends Fee {}
+
+/* 4 */
+interface Foo {}
+class Bar implements Foo {}
+class Baz extends Bar {}
+
+/* 5 */
+interface Zeta {}
+class Alpha implements Zeta {}
+
+interface Beta {}
+class Delta extends Alpha implements Beta {}
